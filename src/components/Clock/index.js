7e8e7042399f9acc513f5fd291c96a7047fd3f59
Bharadwaju Adari <adaribharadwaj@gmail.com>
2021-08-02 07:20:50 +0000

    Realtime Clock using Component Life Cycle

---
+++ src/components/Clock/index.js
@@ -0,0 +1,38 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+    console.log('Constructor called')
+  }
+
+  componentDidMount() {
+    console.log('ComponentDidMount called')
+
+    this.timerID = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    console.log('ComponentWillUnmount called')
+    clearInterval(this.timerID)
+  }
+
+  tick = () => {
+    this.setState({date: new Date()})
+  }
+
+  render() {
+    const {date} = this.state
+    console.log('redber() called')
+    console.log(date)
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+export default Clock
